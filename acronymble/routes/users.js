35201751fa21fd6c90d9d08bb5f8adfe9ebdb10e
Frida Kiriakos <kiriakos.frida@gmail.com>
2015-04-27 20:45:23 -0700

    ignore node_modules from git repo

--- acronymble/routes/users.js
+++ acronymble/routes/users.js
@@ -1,5 +1,10 @@
-var express = require('express');
+#!/usr/bin/env node
+/* jshint node: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, strict: true, undef: true, unused: true */
+"use strict";
+
+var express = require("express");
 var router = express.Router();
+var User = require("../models/user");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
